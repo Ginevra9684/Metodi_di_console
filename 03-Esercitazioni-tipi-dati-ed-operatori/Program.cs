93df48ba5ec5f7c1476c83f68b175354a15cea0f
Ginevra9684 <ginevravallebona98@gmail.com>
2024-06-21 12:56:19 +0200

    05 le condizioni

---
+++ 03-Esercitazioni-tipi-dati-ed-operatori/Program.cs
@@ -0,0 +1,45 @@
+﻿// Pulire il terminale
+
+Console.Clear();
+
+// Stampare il valore di una variabile
+
+int numero;                                              // Dichiaro una variabile di tipo intero
+
+numero = 10;                                             // Inizializzo la variabile numero con il valore 10
+
+Console.WriteLine(numero);                               // Stampo il valore della variabile numero
+
+// Stampare il valore di una variabile con un messaggio
+
+int età = 20;                                            // Dichiaro ed inizializzo la variabile età con il valore 20
+
+Console.WriteLine("L'età è: " + età);                    // Stampo il valore della variabile età con un messaggio
+
+//oppure con interpolazione
+
+Console.WriteLine($"L'Età è: {età}");
+
+//Incrementare il valore di età di una unità
+
+età++;                                                    // incremento il valore della variabile di un'unità
+
+Console.WriteLine($"L'età è: {età}");                     // Stampo il valore della variabile età con un messaggio
+
+//Incrementare l'età di 5 unità
+
+età += 5;                                                 // Incremento il valore della variabile di 5 unità
+
+Console.WriteLine($"L'età è: {età}");                     // Stampo il valore della variabile con un messaggio
+
+// Decrementare l'età di 5 unità
+
+età -= 5;                                                 // Decremento il valore della variabile di 5 unità
+
+Console.WriteLine($"L'età è: {età}"); 
+
+// Stampare due variabili una stringa ed una int
+
+string nome = "Mario";                                    // Dichiaro ed inizializzo la variabile nome con il valore "Mario"
+
+Console.WriteLine($"Il nome è: {nome} e l'età è: {età}"); // Stampo il valore della variabile nome ed età con un messaggio
---
+++ 05-le-condizioni/Program.cs
@@ -0,0 +1,21 @@
+﻿// Un blocco di codice è compreso tra parentesi graffe {} e può contenere uno o più statement
+
+// Le condizioni sono utilizzate per eseguire un blocco di codice solo se una condizione è vera
+
+/*
+Le condizioni possono essere :
+
+if
+if-else
+if-else if-else
+switch
+*/
+
+// if - se il numero è uguale a 10 stampa il numero
+
+int numero = 10;
+
+if (numero == 10)                          // utilizzo l'operatore di confronto == 
+{
+    Console.WriteLine("Il numero è 10");
+}
