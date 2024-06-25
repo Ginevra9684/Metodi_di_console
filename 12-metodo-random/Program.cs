49d337a2892731541365eede214a6541b4232308
Ginevra9684 <ginevravallebona98@gmail.com>
2024-06-25 14:45:51 +0200

    indovina numero

--- 12-metodo-random/Program.cs
+++ 12-metodo-random/Program.cs
@@ -1,5 +1,5 @@
 ﻿// utilizzo di random
 
 Random random = new Random();
-int numero = random.Next(11);     // per fare un intervallo scrivere 2 numeri separati da ,
+int numero = random.Next(10);     // per fare un intervallo scrivere 2 numeri separati da ,
 Console.WriteLine($"Il numero casuale è {numero}");
---
+++ 13-assignment-indovina-numero/Program.cs
@@ -0,0 +1,50 @@
+﻿            //Il programma genera un numero casuale tra 1 - 100
+
+Random random = new Random();
+int number = random.Next(1,101);    
+
+            //Il programma chiede all'utente di inserire un numero
+
+Console.Write("Guess one number included between 1 and 100:\t");
+
+            //comparare il numero casuale con il numero scritto
+
+int chances = 0;
+
+while (chances < 5 )
+{   
+    Console.Write("Guess the number:");
+    int guess = Convert.ToInt32(Console.ReadLine());
+
+        if (guess == number)
+    {
+    Console.WriteLine("Compliments!");
+    }
+        else if (guess < number)
+    {
+        Console.WriteLine("Try a bigger number");
+    }
+        else 
+    {
+        Console.WriteLine("Try a smaller number");
+    }
+
+    chances++;
+}
+
+            //
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
