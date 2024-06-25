aba789acb195a33669e8e0c8e6d8372b702ebeae
Ginevra9684 <ginevravallebona98@gmail.com>
2024-06-25 12:30:36 +0200

    Random

---
+++ 12-metodo-random/Program.cs
@@ -0,0 +1,5 @@
+﻿// utilizzo di random
+
+Random random = new Random();
+int numero = random.Next(11);     // per fare un intervallo scrivere 2 numeri separati da ,
+Console.WriteLine($"Il numero casuale è {numero}");
