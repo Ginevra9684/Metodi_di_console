a9257872f1c80b9cbb3a556fc4296777df3b6943
Ginevra9684 <ginevravallebona98@gmail.com>
2024-06-28 10:22:00 +0200

    string-array

---
+++ 17-string-array/Program.cs
@@ -0,0 +1,10 @@
+﻿string[] nomi = new string[3];
+        nomi[0] = "Mario";
+        nomi[1] = "Luigi";
+        nomi[2] = "Giovanni";
+        //Console.Writeline($"Ciao {nomi[0]}, {nomi[1]} {nomi[2]}");
+        //Stampato tramite for each
+        foreach (string nome in nomi)
+        {
+            Console.WriteLine($"Ciao {nome}");
+        }
