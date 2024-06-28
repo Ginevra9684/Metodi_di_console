e891d345c3e92463a38121ae0c0832d714315ff6
Ginevra9684 <ginevravallebona98@gmail.com>
2024-06-28 10:58:09 +0200

    list int

--- 21-list-int/obj/Debug/net8.0/21-list-int.AssemblyInfo.cs
+++ 21-list-int/obj/Debug/net8.0/21-list-int.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("20-list-string")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("21-list-int")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a3ed97634fafc598ce5a2f222783ab09fb21a7cf")]
-[assembly: System.Reflection.AssemblyProductAttribute("20-list-string")]
-[assembly: System.Reflection.AssemblyTitleAttribute("20-list-string")]
+[assembly: System.Reflection.AssemblyProductAttribute("21-list-int")]
+[assembly: System.Reflection.AssemblyTitleAttribute("21-list-int")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Generato dalla classe WriteCodeFragment di MSBuild.
